2bae6695980ccfb8a06e1706d37c66c0d97ea44c
Tom <thomashenrydavies@hotmail.com>
2022-07-09 22:02:17 +0100

    yeahhh

--- src/User.cpp
+++ src/User.cpp
@@ -2,18 +2,19 @@
 #include "Message.h"
 #include "Connections.h"
 #include "Sender.h"
+#include "Utils.h"
 
 User::User(ENetPeer* peer, Connections* connections) :
     m_peer(peer),
     m_connections(connections)
 {
-    ChangeState <WatingForVersionState>(this);
+    ChangeState <WatingForVersionState>(this);    
 }
 
-void User::DisconnectUser(std::string reason)
+void User::DisconnectUser(const std::string& reason)
 {
     std::cout << "Kick user off: user " << reason << "\n";
-    SendInfoMessage(reason.c_str());
+    SendInfoMessage(reason);
     ChangeState<KickedOffState>();
     enet_peer_disconnect_later(m_peer, 0);
 }
@@ -22,11 +23,11 @@ void User::OnMessage(const Message& msg) {
     m_fsm.ReceiveMessage(msg);
 }
 
-void User::SendInfoMessage(const char* msg) const{
+void User::SendInfoMessage(const std::string& msg) const{
     Message::Make(MessageType::Info, msg).OnData(Sender(m_peer));
 }
 
-bool User::TrySetName(std::string name)
+bool User::TrySetName(const std::string& name)
 {
     if (!m_connections->VerifyName(name))
     {
@@ -34,12 +35,12 @@ bool User::TrySetName(std::string name)
         return false;
     }
 
-    SendInfoMessage("User logged in ok");
+    SendInfoMessage("User logged in ok, your name is " + name + " and your external IP is " + ToString(m_peer->address));
     m_name = name;
     return true;
 }
 
-bool User::TrySetVersion(std::string version)
+bool User::TrySetVersion(const std::string& version)
 {
     if (version.length() == 0)
     {
